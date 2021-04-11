04dfc01d50cb2b526c25584545920f3c30dee398
diana3664 <dianamus2017@gmail.com>
2021-04-11 17:15:36 +0300

    add a responsive function

--- index.js
+++ index.js
@@ -1,70 +1,80 @@
 let getDayBirth, getMonthBirth, getYearBirth, gender;
-
-function getInput() {
- getDayBirth = parseInt(document.getElementById("day-of-birth").value);
- getMonthBirth = parseInt(document.getElementById("month-of-birth").value);
- getYearBirth = document.getElementById("year-of-birth").value
-
 let CC, MM, DD, YY;
 
-  YY = parseInt(getYearBirth.slice(0,2));
-  CC =parseInt(getYearBirth.slice(2,4));
-  console.log('The year of birth:${getyearBirth}')
-  MM= getMonthBirth;
-  DD =getDayBirth;
-}
-function calculate() {
-  let Man = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  let Woman = ["Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  let day0fWeekArr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let d = Math.floor((((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD ) % 7);
+function getInput() {
+  getDayBirth = parseInt(document.getElementById("day-of-birth").value);
+  getMonthBirth = parseInt(document.getElementById("month-of-birth").value);
+  getYearBirth = document.getElementById("year-of-birth").value;
+      
+  YY = parseInt(getYearBirth.substring(0,2));
+  CC = parseInt(getYearBirth.substring(2,4));
+  console.log(`The year of birth: ${getYearBirth}`);
   
-  //  delcare gender
- let radio_buttons = document.getElementById("gender")
- if (radio_buttons[0].checked == true) {
-   gender = "Male"
- }else if (radio_buttons[1].checked){
-   gender = "Female"
- }
- switch (gender) {
-   case "Male":
-     if(d == 0 || d==-0){
-       console.log(`Born on a : ${daOfWeekArr[0]} and your Akan name is ${man[0]}`);
-     }else if (d == 1 || d == -1) {
-      console.log(`Born on a : ${daOfWeekArr[1]} and your Akan name is ${man[1]}`);
-    }else if (d == 2 || d == -2) {
-      console.log(`Born on a : ${daOfWeekArr[2]} and your Akan name is ${man[2]}`);
-    }else if (d == 3 || d == -3) {
-      console.log(`Born on a : ${daOfWeekArr[3]} and your Akan name is ${man[3]}`);
-    }else if (d == 4 || d == -4) {
-      console.log(`Born on a : ${daOfWeekArr[4]} and your Akan name is ${man[4]}`);
-    }else if (d == 5 || d == -5) { 
-      console.log(`Born on a : ${daOfWeekArr[5]} and your Akan name is ${man[5]}`);
-    }else if (d == 6 || d == -6) {
-      console.log(`Born on a : ${daOfWeekArr[6]} and your Akan name is ${man[6]}`);
+  MM = getMonthBirth;
+  DD = getDayBirth;
+  }
+  
+  function calculate() {
+  let masculine_names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  let feminine_names = ["Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  let day0fWeekArr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let d = Math.floor((((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD ) % 7);
+    
+    let radio_buttons = document.getElementsByName("gender");
+    if(radio_buttons[0].checked == true) {
+      gender = "Male"
+    }else if(radio_buttons[1].checked == true) {
+      gender = "Female";
     }
-
-     break;
-    case "Female":
-      if(d == 0 || d==-0){
-        console.log(`Born on a : ${daOfWeekArr[0]} and your Akan name is ${woman[0]}`);
-      }else if (d == 1 || d == -1) {
-       console.log(`Born on a : ${daOfWeekArr[1]} and your Akan name is ${woman[1]}`);
-     }else if (d == 2 || d == -2) {
-       console.log(`Born on a : ${daOfWeekArr[2]} and your Akan name is ${woman[2]}`);
-     }else if (d == 3 || d == -3) {
-       console.log(`Born on a : ${daOfWeekArr[3]} and your Akan name is ${woman[3]}`);
-     }else if (d == 4 || d == -4) {
-       console.log(`Born on a : ${daOfWeekArr[4]} and your Akan name is ${woman[4]}`);
-     }else if (d == 5 || d == -5) { 
-       console.log(`Born on a : ${daOfWeekArr[5]} and your Akan name is ${woman[5]}`);
-     }else if (d == 6 || d == -6) {
-       console.log(`Born on a : ${daOfWeekArr[6]} and your Akan name is ${woman[6]}`);
-     }
-   default:
-     break;
- }
-
-}
+    switch(gender) {
+        case "Male":
+          if (d == 0 || d == -0) {
+              console.log(`Day of birth: ${day0fWeekArr[0]} and your Akan name is ${masculine_names[0]}`);
+            }else if (d == 1 || d == -1) {
+              console.log(`Day of birth: ${day0fWeekArr[1]} and your Akan name is ${masculine_names[1]}`);
+            }else if (d == 2 || d == -2) {
+              console.log(`Day of birth: ${day0fWeekArr[2]} and your Akan name is ${masculine_names[2]}`);
+            }else if (d == 3 || d == -3) {
+              console.log(`Day of birth: ${day0fWeekArr[3]} and your Akan name is ${masculine_names[3]}`);
+            }else if (d == 4 || d == -4) {
+              console.log(`Day of birth: ${day0fWeekArr[4]} and your Akan name is ${masculine_names[4]}`);
+            }else if (d == 5 || d == -5) {
+              console.log(`Day of birth: ${day0fWeekArr[5]} and your Akan name is ${masculine_names[5]}`);
+            }else if (d == 6 || d == -6) {
+              console.log(`Day of birth: ${day0fWeekArr[6]} and your Akan name is ${masculine_names[6]}`);
+            }else if (d >= 7) {
+              alert(`Index out of bounds: ${d}`);
+            }else {
+              alert(`Incorrect Value: ${d}`);
+            }
+            break;
+      case "Female":
+          if (d == 0 || d == -0) {
+              console.log(`Day of birth: ${day0fWeekArr[0]} and your Akan name is ${feminine_names[0]}`);
+            }else if (d == 1 || d == -1) {
+              console.log(`Day of birth: ${day0fWeekArr[1]} and your Akan name is ${feminine_names[1]}`);
+            }else if (d == 2 || d == -2) {
+              console.log(`Day of birth: ${day0fWeekArr[2]} and your Akan name is ${feminine_names[2]}`);
+            }else if (d == 3 || d == -3) {
+              console.log(`Day of birth: ${day0fWeekArr[3]} and your Akan name is ${feminine_names[3]}`);
+            }else if (d == 4 || d == -4) {
+              console.log(`Day of birth: ${day0fWeekArr[4]} and your Akan name is ${feminine_names[4]}`);
+            }else if (d == 5 || d == -5) {
+              console.log(`Day of birth: ${day0fWeekArr[5]} and your Akan name is ${feminine_names[5]}`);
+            }else if (d == 6 || d == -6) {
+              console.log(`Day of birth: ${day0fWeekArr[6]} and your Akan name is ${feminine_names[6]}`);
+            }else if (d >= 7) {
+              alert(`Index out of bounds: ${d}`);
+            }else {
+              alert(`Incorrect Value: ${d}`);
+            }
+      break;
+    }
+  }
+  
+  let run = () => {
+      getInput();
+      calculate();
+  }
 
 
