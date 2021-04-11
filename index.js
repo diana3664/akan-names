37541a433360f0538cce1558cb0588455b09eff4
diana3664 <dianamus2017@gmail.com>
2021-04-11 15:41:10 +0300

    add array for man and woman names in a function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let getDayBirth, getMonthBirth, getYearBirth, gender;
 
 function getInput() {
-let getDayBirth = parseInt(document.getElementById("day-of-birth").value);
-let getMonthBirth = parseInt(document.getElementById("month-of-birth").value);
-let getYearBirth = document.getElementById("year-of-birth").value
+ getDayBirth = parseInt(document.getElementById("day-of-birth").value);
+ getMonthBirth = parseInt(document.getElementById("month-of-birth").value);
+ getYearBirth = document.getElementById("year-of-birth").value
 let male = document.getElementById("male-gender").value
 let Female = document.getElementById("female-gender").value
 let day0fWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -12,20 +12,16 @@ let day0fWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturd
 formular =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 ;
 let CC, MM, DD, YY;
 
-  YY = getYearBirth.slice(0,2);
-  CC =getYearBirth.slice(2,4);
-  MM= parseInt(getMonthBirth);
-  DD = parseInt(getDayBirth);
-
-  console.log(`${CC}`);
-  console.log(`${YY}`);
-  console.log(`${MM}`);
-  console.log(`${DD}`);
+  YY = parseInt(getYearBirth.slice(0,2));
+  CC =parseInt(getYearBirth.slice(2,4));
+  console.log('The year of birth:${getyearBirth}')
+  MM= getMonthBirth;
+  DD =getDayBirth;
 }
 function calculate() {
-  dayOfWeek = formular;
-  day0fWeek = Math.floor(day0fWeek);
-  console.log(`${day0fWeek}`);
+  let Man = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  let Woman = ["Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
 }
 
 
