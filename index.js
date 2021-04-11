7e3f1ccd9a3ea6a8232bdb0b53d3c849ef08d24a
diana3664 <dianamus2017@gmail.com>
2021-04-11 16:00:36 +0300

    declare button for checking male and female

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ function getInput() {
  getDayBirth = parseInt(document.getElementById("day-of-birth").value);
  getMonthBirth = parseInt(document.getElementById("month-of-birth").value);
  getYearBirth = document.getElementById("year-of-birth").value
-let male = document.getElementById("male-gender").value
-let Female = document.getElementById("female-gender").value
 
-
-formular =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 ;
 let CC, MM, DD, YY;
 
   YY = parseInt(getYearBirth.slice(0,2));
@@ -24,7 +20,13 @@ function calculate() {
   let d = Math.floor((((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD ) % 7);
   
   //  delcare gender
-  
+ let radio_buttons = document.getElementById("gender")
+ if (radio_buttons[0].checked == true) {
+   gender = "Male"
+ }else if (radio_buttons[1].checked){
+   gender = "Female"
+ }
+
 }
 
 
