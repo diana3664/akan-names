21bd387da59110a06df56b36ea7142f506449184
diana3664 <dianamus2017@gmail.com>
2021-04-11 15:26:45 +0300

    move the getInput function to call also the variable

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var getDayBirth = document.getElementById("day-of-birth").value
-var getMonthBirth = document.getElementById("month-of-birth").value
-var getYearBirth = document.getElementById("year-of-birth").value
-var male = document.getElementById("male-gender").value
-var Female = document.getElementById("female-gender").value
-var day0fWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let getDayBirth, getMonthBirth, getYearBirth, gender;
+
+function getInput() {
+let getDayBirth = document.getElementById("day-of-birth").value
+let getMonthBirth = document.getElementById("month-of-birth").value
+let getYearBirth = document.getElementById("year-of-birth").value
+let male = document.getElementById("male-gender").value
+let Female = document.getElementById("female-gender").value
+let day0fWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
 formular =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 ;
-let raw_year, CC, MM, DD, YY, day_of_week;
+let CC, MM, DD, YY;
 
-function getInput() {
   YY = getYearBirth.slice(0,2);
   CC =getYearBirth.slice(2,4);
   MM= parseInt(getMonthBirth);
@@ -19,6 +21,11 @@ function getInput() {
   console.log(`${YY}`);
   console.log(`${MM}`);
   console.log(`${DD}`);
+}
+function calculate() {
+  dayOfWeek = formular;
+  day0fWeek = Math.floor(day0fWeek);
+  console.log(`${day0fWeek}`);
 }
 
 
