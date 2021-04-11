ed4fb67e682ff57604e92cc71466fa99539174ce
diana3664 <dianamus2017@gmail.com>
2021-04-11 11:09:16 +0300

    declare the day,month,year,male,female variable

---
+++ index.js
@@ -0,0 +1,7 @@
+var getDayBirth = document.getElementById("day-of-birth").innerHTML
+var getMonthBirth = document.getElementById("month-of-birth").innerHTML
+var getYearBirth = document.getElementById("year-of-birth").innerHTML
+var male = document.getElementById("male-gender").innerHTML
+var Female = document.getElementById("female-gender").innerHTML
+
+
